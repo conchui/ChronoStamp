d74bfcb0d6ce144e0bdafb35f6e54571cb8e04a1
Rhodel Conchui Perez <conchui.perez@gmail.com>
2014-12-17 14:15:54 +0800

    edited some items

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @tasks = current_user.tasks_working
     @projects = current_user.project
     year = 2013
-
+    @holidays = Holiday.where("YEAR(holiday_date) = ?", year)
   end
 
 
